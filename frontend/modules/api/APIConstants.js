ea3bea469b5e41c160fd05d01891283803d7902e
patilvinayb9 <patilvinayb9@gmail.com>
2024-01-03 10:07:32 +0100

    Server url base url added

--- frontend/modules/api/APIConstants.js
+++ frontend/modules/api/APIConstants.js
@@ -1,14 +1,16 @@
 
+BASE_URI = "http://localhost:8080";
+
 // UserController
 
-URL_CREATE_USER = "api/unsecured/core/createUser";
-URL_CREATE_ADMIN = "api/secured/core/createAdmin";
+URL_CREATE_USER = BASE_URI + "/api/unsecured/core/createUser";
+URL_CREATE_ADMIN = BASE_URI + "/api/secured/core/createAdmin";
 
-URL_RESET_PASSWORD = "api/unsecured/core/resetPassword";
-URL_LOGIN_USER = "api/unsecured/core/getLoggedInUser";
-URL_LOGIN_ROLE = "api/secured/core/getLoggedInRole";
-URL_AUTH_FAILED = "api/unsecured/core/authFailed";
+URL_RESET_PASSWORD = BASE_URI + "/api/unsecured/core/resetPassword";
+URL_LOGIN_USER = BASE_URI + "/api/unsecured/core/getLoggedInUser";
+URL_LOGIN_ROLE = BASE_URI + "/api/secured/core/getLoggedInRole";
+URL_AUTH_FAILED = BASE_URI + "/api/unsecured/core/authFailed";
 
-URL_VERIFICATION_CODE = "api/unsecured/core/sendVerificationCode";
+URL_VERIFICATION_CODE = BASE_URI + "/api/unsecured/core/sendVerificationCode";
 
 
